da597d4602b8d1004ad4855e05fbc34b19d71a18
Sofia Sgroi <162578481+SofiaSgroi@users.noreply.github.com>
2025-06-24 15:55:34 +0200

    Delete Fixed_Length_GA/results/genericAncillas_beta1tobeta4_tc05Nc10 directory

--- Fixed_Length_GA/results/genericAncillas_beta1tobeta4_tc05Nc10/info/hyperparameters_GA.py
+++
@@ -1,5 +0,0 @@
-pop_size = 200
-p_new = 0.05
-K = 4
-nbests = 100
-mutation_factor = 1.
--- Fixed_Length_GA/results/genericAncillas_beta1tobeta4_tc05Nc10/info/hyperparameters_physics.py
+++
@@ -1,6 +0,0 @@
-N_levs = 20
-beta_range = 5
-
-npdtype="complex64"
-objective_precision = 6 #number of decimals
-
