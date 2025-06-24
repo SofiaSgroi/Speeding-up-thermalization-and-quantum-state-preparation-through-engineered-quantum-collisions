9fdb4b8bbb3e19377aead0d651cc67a95366ec9a
Sofia Sgroi <162578481+SofiaSgroi@users.noreply.github.com>
2025-06-24 16:13:09 +0200

    Add files via upload

---
+++ Fixed_Length_GA/results/thermalAncillas_n0tocoherent1_tc05Nc10/info/config.py
@@ -0,0 +1,14 @@
+folder_name ="thermalAncillas_n0tocoherent1_tc05Nc10"
+nsteps = 1000
+
+thermal_ancillas = True
+
+Omega_S = 1.
+rho0_tuple = ("number", 0)
+t_c = 0.5
+N_c = 10
+
+g = 1.
+
+target = [("number", 0), ("D", 1.)]
+
