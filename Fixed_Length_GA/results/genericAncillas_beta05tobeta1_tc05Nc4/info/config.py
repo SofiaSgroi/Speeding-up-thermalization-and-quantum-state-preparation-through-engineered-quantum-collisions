ef2e14cb389703f5cb3226adc82195f5bd3f1ee6
Sofia Sgroi <162578481+SofiaSgroi@users.noreply.github.com>
2025-06-24 15:58:37 +0200

    Add files via upload

---
+++ Fixed_Length_GA/results/genericAncillas_beta05tobeta1_tc05Nc4/info/config.py
@@ -0,0 +1,14 @@
+folder_name ="genericAncillas_beta05tobeta1_tc05Nc4"
+nsteps = 1000
+
+thermal_ancillas = False
+
+Omega_S = 1.
+rho0_tuple = ("thermal", 0.5)
+t_c = 0.5
+N_c = 4
+
+g = 1.
+
+target = [("thermal", 1.)]
+
---
+++ Fixed_Length_GA/results/genericAncillas_beta05ton0_tc05Nc2/info/config.py
@@ -0,0 +1,14 @@
+folder_name ="genericAncillas_beta05ton0_tc05Nc2"
+nsteps = 1000
+
+thermal_ancillas = False
+
+Omega_S = 1.
+rho0_tuple = ("thermal", 0.5)
+t_c = 0.5
+N_c = 2
+
+g = 1.
+
+target = [("number", 0)]
+
